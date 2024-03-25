07480ef68c7052df8096834137d840c829e5ccc5
Raitis ZE <raitis.zgirskis@gmail.com>
2024-03-25 23:01:29 +0200

    Day 3 with enums

--- 100DaysOfSwiftUI.playground/Contents.swift
+++ 100DaysOfSwiftUI.playground/Contents.swift
@@ -176,6 +176,30 @@ archEnemies["Superman"] = "Lex Luthor"
 archEnemies["Batman"] = "Penguin"
 
 // Enumeration - enums
+var selected = "Monday"
+selected = "Tuesday"
+selected = "January"
+selected = "Friday "
+
+//enum Weekday {
+//    case monday
+//    case tuesday
+//    case wednesday
+//    case thursday
+//    case friday
+//}
+
+// var day = Weekday.monday
+// day = Weekday.tuesday
+//day = Weekday.friday
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = .tuesday
+day = .friday
 
 
 // DAY 4  Arrays
